7f2024e35237bd8e9eb4b7e9ee7bd1b4319b4220
Jaime Fullana <jaimef1603@gmail.com>
2018-10-23 19:22:56 +0200

    "Added the ArrayList and VirtualCampus classes, started the creation of Administrator's constructor, created the pointers to data arrays of administrator class and included the requiered includes in some classes.

--- Administrator.cpp
+++ Administrator.cpp
@@ -1,6 +1,17 @@
-#include "Administrator.h"
+﻿#include "Administrator.h"
 
-Administrator::Administrator()
+Administrator::Administrator():Professor()
 {
+
+}
+
+Administrator::Administrator(string ident, const Administrator &admin):Professor(ident){
+
+    AdminPtr=admin.AdminPtr;
+    profesorPtr=admin.profesorPtr;
+    studentPtr=admin.studentPtr;
+    coursePtr=admin.coursePtr;
+    seminarPtr=admin.seminarPtr;
+    fdpPtr=admin.fdpPtr;
 
 }

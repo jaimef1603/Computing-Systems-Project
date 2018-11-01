300cb1fad375b405e64bd0291fe21b44c36aa352
Jaime Fullana <jaimef1603@gmail.com>
2018-11-01 19:14:25 +0100

    Student and Course class main structures have been almost finished. The User abstract class has been added, the resource class has been made abstract and the relationship class Link_us_res has been added. The project has turned a bit into a more polymorphic approach and the the work on implementation of the "enroll" mechanism has started.

---
+++ Link_us_res.cpp
@@ -0,0 +1,6 @@
+#include "Link_us_res.h"
+
+Link_us_res::Link_us_res()
+{
+
+}
---
+++ Link_us_res.h
@@ -0,0 +1,11 @@
+#ifndef LINK_US_RES_H
+#define LINK_US_RES_H
+
+
+class Link_us_res
+{
+public:
+    Link_us_res();
+};
+
+#endif // LINK_US_RES_H
